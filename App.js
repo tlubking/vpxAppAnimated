dd93678250132271f7313544bb8a55968b6350fa
tlubking <trevor.lubking@gmail.com>
2020-06-16 21:25:30 -0700

    First Commit

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Asset } from "expo-asset";
+import { AppLoading } from "expo";
+import VpxApp from "./app/index.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsyn();
+    }
+  });
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([require("./assets/vpxBack.png")]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <VpxApp />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
